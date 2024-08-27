b056992f9a51b3fd98dc508af6c79e6894f5b102
Autumn Eckley <eckley.autumn@gmail.com>
2024-08-27 14:17:06 -0600

    Starting snack looks

--- vendr/app/AppState.js
+++ vendr/app/AppState.js
@@ -1,8 +1,14 @@
+import { Snack } from './models/Snacks.js'
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
 
+  snacks = [
+    new Snack({name: 'Snickers', price: 3.50, imgUrl: 'https://www.pngall.com/wp-content/uploads/8/Sample-Watermark-Transparent.png'})
+   ]
+   
+   money = 0
  
 }
 
